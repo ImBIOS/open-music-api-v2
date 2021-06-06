5ecde067edf632c8e3e2295e1ee7dd57f48f7e9f
Imamuzzaki <imamuzzaki@gmail.com>
2021-06-06 21:30:59 +0700

    authentication implemented

--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -3,26 +3,41 @@ const routes = (handler) => [
     method: "POST",
     path: "/songs",
     handler: handler.postSongHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
     method: "GET",
     path: "/songs",
     handler: handler.getSongsHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
     method: "GET",
     path: "/songs/{id}",
     handler: handler.getSongByIdHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
     method: "PUT",
     path: "/songs/{id}",
     handler: handler.putSongByIdHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
   {
     method: "DELETE",
     path: "/songs/{id}",
     handler: handler.deleteSongByIdHandler,
+    options: {
+      auth: "songsapp_jwt",
+    },
   },
 ];
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const Jwt = require("@hapi/jwt");
 
 // songs
 const songs = require("./api/songs");
@@ -13,9 +14,17 @@ const users = require("./api/users");
 const UsersService = require("./services/postgres/UsersService");
 const UsersValidator = require("./validator/users");
 
+// authentications
+const authentications = require("./api/authentications");
+const AuthenticationsService = require("./services/postgres/AuthenticationsService");
+const TokenManager = require("./tokenize/TokenManager");
+const AuthenticationsValidator = require("./validator/authentications");
+
 const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -24,6 +33,30 @@ const init = async () => {
         origin: ["*"],
       },
     },
+  });
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy("songsapp_jwt", "jwt", {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register({
@@ -38,6 +71,15 @@ const init = async () => {
     options: {
       service: usersService,
       validator: UsersValidator,
+    },
+  },
+  {
+    plugin: authentications,
+    options: {
+      authenticationsService,
+      usersService,
+      tokenManager: TokenManager,
+      validator: AuthenticationsValidator,
     },
   });
 
