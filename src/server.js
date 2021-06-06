3a02977fa9e0b6cf89fd3f204fcd9272791903aa
Imamuzzaki <imamuzzaki@gmail.com>
2021-06-06 21:11:05 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,48 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require("dotenv").config();
+
+const Hapi = require("@hapi/hapi");
+
+// songs
+const songs = require("./api/songs");
+const SongsService = require("./services/postgres/SongsService");
+const SongsValidator = require("./validator/songs");
+
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
+const init = async () => {
+  const songsService = new SongsService();
+  const usersService = new UsersService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
+    },
+  },
+  {
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
