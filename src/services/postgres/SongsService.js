9789bbd17f94636c3555952a18da2914fde0694e
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2021-08-28 09:52:47 +0700

    fix inconsistency

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -42,7 +42,7 @@ class SongsService {
     };
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError("Lagu tidak ditemukan");
     }
 
@@ -60,7 +60,7 @@ class SongsService {
 
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError("Gagal memperbarui lagu. Id tidak ditemukan");
     }
   }
@@ -73,7 +73,7 @@ class SongsService {
 
     const result = await this._pool.query(query);
 
-    if (!result.rows.length) {
+    if (!result.rowCount) {
       throw new NotFoundError("Lagu gagal dihapus. Id tidak ditemukan");
     }
   }
